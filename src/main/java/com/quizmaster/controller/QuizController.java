64c721cec835b26cd6e31b39cec23c37a1da8f15
Anuj Sandeep Wagh <anuj_wagh_comp@moderncoe.edu.in>
2026-04-23 11:57:40 +0530

    Update QuizController.java

--- src/main/java/com/quizmaster/controller/QuizController.java
+++ src/main/java/com/quizmaster/controller/QuizController.java
@@ -1,7 +1,6 @@
 package com.quizmaster.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,34 +9,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.quizmaster.entity.Quiz;
 import com.quizmaster.service.QuizService;
-
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/createquiz")
+@RequestMapping("/api/quiz")
 public class QuizController {
-	@Autowired
-	private QuizService qs;
-	
-	@PostMapping
-	public Quiz createQuiz(@Valid @RequestBody Quiz q) {
-		return qs.createQuiz(q);
-	}
-	@GetMapping
-	public List<Quiz> getAllQuiz(){
-		return qs.getAllQuiz();
-	}
-	
-	@GetMapping("/{id}")
-    public Quiz getQuizById(@PathVariable Long id) {
-        return qs.getQuizById(id);               
+
+    @Autowired
+    private QuizService qs;
+
+    @PostMapping("/create")
+    public Quiz createQuiz(@Valid @RequestBody Quiz q) {
+        return qs.createQuiz(q);
     }
-	 @DeleteMapping("/{id}")
-	    public String deleteQuiz(@PathVariable Long id) {
-	       qs.deleteQuiz(id);
-	        return "Quiz deleted successfully";
-	    }
+
+    @GetMapping("/all")
+    public List<Quiz> getAllQuiz() {
+        return qs.getAllQuiz();
+    }
+
+    @GetMapping("/{id}")
+    public Quiz getQuizById(@PathVariable Long id) {
+        return qs.getQuizById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteQuiz(@PathVariable Long id) {
+        qs.deleteQuiz(id);
+        return "Quiz deleted successfully";
+    }
 }
