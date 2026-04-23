e1f8cf4e48fe7e4d302a42c2937743bdbdcb8351
Anuj Sandeep Wagh <anuj_wagh_comp@moderncoe.edu.in>
2026-04-23 12:03:18 +0530

    Update UserQuizController.java

--- src/main/java/com/quizmaster/controller/UserQuizController.java
+++ src/main/java/com/quizmaster/controller/UserQuizController.java
@@ -1,6 +1,6 @@
 package com.quizmaster.controller;
 
-	import java.util.List;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,16 +12,16 @@ import com.quizmaster.service.UserQuizService;
 @RequestMapping("/api/quiz")
 public class UserQuizController {
 
-	@Autowired
-	private UserQuizService userQuizService;
+    @Autowired
+    private UserQuizService userQuizService;
 
-	@GetMapping
-	public ResponseEntity<List<QuizResponseDTO>> getAllQuizzes() {
-		return ResponseEntity.ok(userQuizService.getAllQuizzes());
-	}
+    @GetMapping("/all")
+    public ResponseEntity<List<QuizResponseDTO>> getAllQuizzes() {
+        return ResponseEntity.ok(userQuizService.getAllQuizzes());
+    }
 
-	@GetMapping("/{quizId}/start")
-	public ResponseEntity<List<QuestionResponseDTO>> startQuiz(@PathVariable Long quizId) {
-		return ResponseEntity.ok(userQuizService.startQuiz(quizId));
-	}
+    @GetMapping("/{quizId}/start")
+    public ResponseEntity<List<QuestionResponseDTO>> startQuiz(@PathVariable Long quizId) {
+        return ResponseEntity.ok(userQuizService.startQuiz(quizId));
+    }
 }
