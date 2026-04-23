a5982574bdefa6c642d7af90e2db652ee758f4fe
Anuj Sandeep Wagh <anuj_wagh_comp@moderncoe.edu.in>
2026-04-23 12:02:39 +0530

    Update ResultController.java

--- src/main/java/com/quizmaster/controller/ResultController.java
+++ src/main/java/com/quizmaster/controller/ResultController.java
@@ -1,7 +1,6 @@
 package com.quizmaster.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,23 +10,22 @@ import com.quizmaster.entity.Result;
 import com.quizmaster.service.ResultService;
 
 @RestController
-@RequestMapping("/api/quiz")
 public class ResultController {
 
     @Autowired
     private ResultService resultService;
 
-    @PostMapping("/submit")
+    @PostMapping("/api/attempt/submit")
     public ResponseEntity<SubmitResponseDTO> submitQuiz(@RequestBody SubmitRequestDTO dto) {
         return ResponseEntity.ok(resultService.submitQuiz(dto));
     }
-    
-    @GetMapping("/leaderboard/{quizId}")
+
+    @GetMapping("/api/leaderboard/{quizId}")
     public ResponseEntity<List<Result>> getLeaderboard(@PathVariable Long quizId) {
         return ResponseEntity.ok(resultService.getLeaderboard(quizId));
     }
 
-    @GetMapping("/history/{userId}")
+    @GetMapping("/api/result/user/{userId}")
     public ResponseEntity<List<Result>> getMyResults(@PathVariable Long userId) {
         return ResponseEntity.ok(resultService.getMyResults(userId));
     }
