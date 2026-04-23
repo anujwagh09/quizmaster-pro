853a964a391f5b957d8bfb8f2722cad09b32a3fb
Anuj Sandeep Wagh <anuj_wagh_comp@moderncoe.edu.in>
2026-04-23 12:01:11 +0530

    Update QuizController.java

--- src/main/java/com/quizmaster/controller/QuizController.java
+++ src/main/java/com/quizmaster/controller/QuizController.java
@@ -2,13 +2,7 @@ package com.quizmaster.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.quizmaster.entity.Quiz;
 import com.quizmaster.service.QuizService;
 import jakarta.validation.Valid;
