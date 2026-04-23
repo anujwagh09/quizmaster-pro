d670eaa289db65f9039b6c4ba57ba0200c905297
Anuj Sandeep Wagh <anuj_wagh_comp@moderncoe.edu.in>
2026-04-23 12:02:00 +0530

    Update QuestionController.java

--- src/main/java/com/quizmaster/controller/QuestionController.java
+++ src/main/java/com/quizmaster/controller/QuestionController.java
@@ -1,47 +1,39 @@
 package com.quizmaster.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.quizmaster.entity.Question;
 import com.quizmaster.service.QuestionService;
-
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/questions")
+@RequestMapping("/api/question")
 public class QuestionController {
-	@Autowired
-	private QuestionService qs;
 
-	@PostMapping("/quiz/{quizId}")
-	public Question addQuestions(@PathVariable Long quizId, @Valid @RequestBody Question question) {
-		return qs.addQuestion(quizId, question);
-	}
+    @Autowired
+    private QuestionService qs;
 
-	@GetMapping("/quiz/{quizId}")
-	public List<Question> getQuestionsByQuiz(@PathVariable Long quizId) {
-		return qs.getQuestionsByQuizId(quizId);
-	}
+    @PostMapping("/quiz/{quizId}")
+    public Question addQuestion(@PathVariable Long quizId,
+                                @Valid @RequestBody Question question) {
+        return qs.addQuestion(quizId, question);
+    }
 
-	@PutMapping("/{id}")
-	public Question updateQuestion(@PathVariable Long id, @RequestBody Question question) {
-		return qs.updateQuestion(id, question);
-	}
+    @GetMapping("/quiz/{quizId}")
+    public List<Question> getQuestionsByQuiz(@PathVariable Long quizId) {
+        return qs.getQuestionsByQuizId(quizId);
+    }
 
-	@DeleteMapping("/{id}")
-	public String deleteQuestion(@PathVariable Long id) {
-		qs.deleteQuestion(id);
-		return "Question deleted successfully";
-	}
+    @PutMapping("/{id}")
+    public Question updateQuestion(@PathVariable Long id,
+                                   @RequestBody Question question) {
+        return qs.updateQuestion(id, question);
+    }
 
+    @DeleteMapping("/{id}")
+    public String deleteQuestion(@PathVariable Long id) {
+        qs.deleteQuestion(id);
+        return "Question deleted successfully";
+    }
 }
